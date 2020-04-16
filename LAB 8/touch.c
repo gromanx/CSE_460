31bb1867dc323669b233c0e1ce76b405c18662c3
Gladys Roman <54556129+gromanx@users.noreply.github.com>
2020-04-16 13:54:47 -0700

    Add files via upload

--- LAB 8/touch.c
+++ LAB 8/touch.c
@@ -1,1 +1,21 @@
+/* Gladys Roman
+ * CSE460 
+ * touch.c
+ */
 
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char *argv[]){
+	int n = argc;
+	for(int i = 1 ; i <= n ; i++){
+		if(open(argv[i], O_CREATE) < 0){
+			printf(i, "cp: cannout create %s \n", argv[i]);
+			exit();
+		}
+	}
+}
+			
